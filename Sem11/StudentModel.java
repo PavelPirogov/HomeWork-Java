c5c18e13145c16e53c7b243d30e15d1717384257
Петр <televizor50@yandex.ru>
2023-07-22 18:27:39 +0300

    oop home 11

---
+++ Sem11/StudentModel.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class StudentModel {
+        private List<Student> students = new ArrayList<>();
+
+        public void create(String name, UUID groupId) {
+            students.add(new Student(name, groupId));
+        }
+
+        public List<Student> getAll() {
+            return List.copyOf(students);
+        }
+
+    }
